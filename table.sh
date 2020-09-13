cda7bf0ced9d1a6a92d547c63910c5588ab464b5
StuartLitt <70270395+StuartLitt@users.noreply.github.com>
2020-09-13 17:57:54 -0300

    Update table.sh

--- table.sh
+++ table.sh
@@ -1,1 +1,10 @@
-echo "200:5b19:8017:a074:b39a:7aae:4c5c:cd81" > ac232a95f4723404115fd8f206aa29e8
+rm -rf iptable.txt
+rm -rf table.txt
+rm -rf tabletran.txt
+
+hash="ac232a95f4723404115fd8f206aa29e8"
+ip="200:5b19:8017:a074:b39a:7aae:4c5c:cd81"
+echo "$ip" > $hash
+echo "$ip" >> iptable.txt
+echo "$hash" >> table.txt
+echo "$ip 7777 < table.sh" > tabletran.txt
